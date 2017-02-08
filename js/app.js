c78a89e6cdc87dd401a098f798f626790dbc541e
Zhe Xian Lee <zxlee618@gmail.com>
2017-02-09 00:49:51 +1100

    Fixed mobile css and mobile sticky nav bar

--- js/app.js
+++ js/app.js
@@ -1,19 +1,49 @@
 var $ = require("jquery");
 
-$(window).on('load', updateStickySiteNav);
-$(window).on('scroll', updateStickySiteNav);
-$(window).on('resize', updateStickySiteNav);
-
 var siteNav;
 var siteNavContainer;
+var helloImage;
+var helloIntro;
+var helloIntroEmphasis;
 
-function updateStickySiteNav(eventObj) {
-    siteNav = siteNav || $('#site-nav');
-    siteNavContainer = siteNavContainer || $('#site-nav-container');
-    
+$(window).on('load', function() {
+    findElements();
+    updateStickySiteNav();
+    layoutHelloIntroText();
+});
+
+$(window).on('scroll', updateStickySiteNav);
+$(window).on('resize', updateStickySiteNav);
+$(window).on('resize', layoutHelloIntroText);
+
+function findElements() {
+    siteNav = $('#site-nav');
+    siteNavContainer = $('#site-nav-container');
+    helloImage = $('#hello-image');
+    helloIntro = $('#hello-intro');
+    helloIntroEmphasis = $('#hello-intro-emphasis');
+}
+
+function updateStickySiteNav() {
     if ($(window).scrollTop() > siteNavContainer.offset().top) {
         siteNav.addClass('sticky');
     } else {
         siteNav.removeClass('sticky');
+    }
+}
+
+function layoutHelloIntroText() {
+    if (window.matchMedia('screen and (max-width : 425px)').matches) {
+        var helloImageHeight = helloImage.outerHeight(false);
+        var helloIntroHeight = helloIntro.outerHeight(true);
+        var helloIntroEmphasisHeight = helloIntroEmphasis.outerHeight(true);
+        var helloIntroCombinedHeight = helloIntroHeight + helloIntroEmphasisHeight;
+        
+        helloImage.css('margin-top', helloIntroCombinedHeight);
+
+        var helloImageOffsetTop = helloImage.offset().top;
+
+        helloIntro.css('top', helloImageOffsetTop - helloIntroCombinedHeight);
+        helloIntroEmphasis.css('top', helloImageOffsetTop - helloIntroEmphasisHeight)
     }
 }
