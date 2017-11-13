54dc8f3c5c69fcb7a6d368aad044a1f376d3aa63
Zhe Xian Lee <zxlee@zhexians-mbp.lan.uow.edu.au>
2017-11-13 13:31:06 +1100

    fix redirect

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-var finalhandler = require('finalhandler')
-var http = require('http')
-var serveStatic = require('serve-static')
+// var finalhandler = require('finalhandler')
+// var http = require('http')
+// var serveStatic = require('serve-static')
 
-// Serve up public/ftp folder
-var serve = serveStatic('public', {'index': ['index.html', 'index.htm']})
+// // Serve up public/ftp folder
+// var serve = serveStatic('public', {'index': ['index.html', 'index.htm']})
 
-// Create server
-var server = http.createServer(function onRequest (req, res) {
-  // serve(req, res, finalhandler(req, res))
-  res.redirect(301, 'https://evon-portfolio.firebaseapp.com');
-})
+// // Create server
+// var server = http.createServer(function onRequest (req, res) {
+//   // serve(req, res, finalhandler(req, res))
+//   // res.redirect(301, 'https://evon-portfolio.firebaseapp.com');
+// })
 
-// Listen
-server.listen(process.env.PORT || 8080)
+// // Listen
+// server.listen(process.env.PORT || 8080)
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.redirect(301, 'https://evon-portfolio.firebaseapp.com'));
+
+app.listen(process.env.PORT || 8080);
